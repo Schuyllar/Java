966eb39c49e9b59e399a022c17a86b11bfa4596a
Schuyllar <s.frantz2024@gmail.com>
2024-11-21 11:23:20 -0600

    my first changes

---
+++ src/main/java/org/example/TheMeaningOfLife.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import javax.swing.*;
+
+public class TheMeaningOfLife {
+    public static void main(String[] args) {
+        // Single-line comment in Java
+
+        /*
+        * Multi-line comment in Java
+        *
+        *
+        *
+        * */
+        System.out.println("Your random number is: " + (1 + (int) (Math.random() * 10)));
+    }
+}
